c43f7b3ff30d28f01cf7b996c33cb4f5f38a3986
KevinLiTian <kevin.li20021106@gmail.com>
2023-02-10 12:16:10 -0500

    process

--- models/rollout.py
+++ models/rollout.py
@@ -6,16 +6,13 @@ import torch.nn.functional as F
 class GoRolloutNetwork(nn.Module):
     def __init__(self):
         super(GoRolloutNetwork, self).__init__()
-
-        # Conv layer
         self.conv = nn.Conv2d(4, 1, kernel_size=(1, 1), stride=1)
 
         # Bias
         self.bias = nn.Parameter(torch.zeros(361))
 
     def forward(self, x):
-        # Pass the input through the convolutional layers
-        x = F.relu(self.conv(x))
+        x = self.conv(x)
 
         # Flatten
         x = x.view(-1, 19 * 19)
